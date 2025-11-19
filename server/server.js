4a2421d8aac8fd3c8b7001497ce95a64091ee613
Loraine Mheriz Espartero <espartero.cpsu@gmail.com>
2025-11-19 08:31:43 +0800

    Add server folder

---
+++ server/server.js
@@ -0,0 +1,95 @@
+import express from "express";
+import mysql from "mysql2";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "dessertify",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("atabase connection failed:", err);
+  } else {
+    console.log("Connected to MySQL database");
+  }
+});
+
+app.get("/api/recipes", (_req, res) => {
+  db.query("SELECT * FROM desserts", (err, results) => {
+    if (err) return res.status(500).json({ error: err });
+    res.json(results);
+  });
+});
+
+app.post("/api/recipes", (req, res) => {
+  const {
+    dessert_name,
+    description,
+    category,
+    image,
+    difficulty,
+    preparation_time,
+    cooking_time,
+    serving_size,
+    ingredients,
+    steps,
+  } = req.body;
+
+  const sql = `
+    INSERT INTO desserts
+    (dessert_name, description, category, image, difficulty, preparation_time, cooking_time, serving_size, ingredients, steps)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+
+  db.query(
+    sql,
+    [
+      dessert_name,
+      description,
+      category,
+      image,
+      difficulty,
+      preparation_time,
+      cooking_time,
+      serving_size,
+      ingredients,
+      steps,
+    ],
+    (err, result) => {
+      if (err) return res.status(500).json({ error: err });
+      res.json({ id: result.insertId, ...req.body });
+    }
+  );
+});
+
+app.delete("/api/recipes/:id", (req, res) => {
+  const { id } = req.params;
+  const sql = "DELETE FROM desserts WHERE id = ?";
+
+  db.query(sql, [id], (err, result) => {
+    if (err) return res.status(500).json({ error: err });
+    res.json({ success: true });
+  });
+});
+
+app.get("/api/desserts/:category", (req, res) => {
+  const category = req.params.category;
+  const sql = "SELECT * FROM desserts WHERE category = ?";
+  db.query(sql, [category], (err, result) => {
+    if (err) return res.status(500).json({ error: "Database error" });
+    res.json(result);
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
